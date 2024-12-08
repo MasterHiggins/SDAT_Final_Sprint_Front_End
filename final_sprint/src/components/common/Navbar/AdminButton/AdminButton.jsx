075e4acc52c1a008d52ca392d4224475ccfa4280
Evan <evan.pittman@keyin.com>
2024-12-08 14:11:19 -0330

    added a basic admin page

--- final_sprint/src/components/common/Navbar/AdminButton/AdminButton.jsx
+++ final_sprint/src/components/common/Navbar/AdminButton/AdminButton.jsx
@@ -1,17 +1,27 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FaUserCog } from "react-icons/fa";
 import styles from "./AdminButton.module.css";
 
 function AdminButton() {
+  const navigate = useNavigate();
+
+  const handleClick = (e)=>{
+    e.preventDefault();
+    console.log("button clicked")
+    navigate('/admin')
+  }
+
   return (
-    <Link
-      to="/admin"
+
+      <button 
+      onClick={handleClick}
       className={styles.adminButton}
-      aria-label="Admin Dashboard"
-    >
+      aria-label="Admin Page"
+      >
       <FaUserCog className={styles.icon} />
       <span>Admin</span>
-    </Link>
+      </button>
   );
 }
 
