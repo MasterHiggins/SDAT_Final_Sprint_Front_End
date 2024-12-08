9ec2b962fe9e85eadea8ba50eac33974b8d411eb
Maz Radwan <maz.radwan@keyin.com>
2024-12-08 14:24:32 -0330

    search flight menu added w/o api

--- final_sprint/src/App.js
+++ final_sprint/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar/Navbar";
 import FlightBoard from "./components/public/FlightBoard/FlightBoard";
+import FlightSearch from "./components/public/FlightSearch/FlightSearch";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<FlightBoard />} />
+          <Route path="/search" element={<FlightSearch />} />
         </Routes>
       </div>
     </Router>
