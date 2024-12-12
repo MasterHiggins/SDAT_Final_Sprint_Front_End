453ff76dc279978c6ab297f3507947c7ae714ac4
Maz Radwan <maz.radwan@keyin.com>
2024-12-12 09:27:20 -0330

    fixed button styling in airport

--- final_sprint/src/components/admin/airport/airportTable/AirportTable.jsx
+++ final_sprint/src/components/admin/airport/airportTable/AirportTable.jsx
@@ -1,57 +1,46 @@
-import { FaEdit,FaTrash } from "react-icons/fa";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import styles from "./AirportTable.module.css";
 
+const AirportTable = ({ airports, onEdit, onDelete, onFlighView }) => {
+  if (!airports || airports.length === 0) {
+    return <p>no airports</p>;
+  }
 
-
-
-const AirportTable =({airports,onEdit,onDelete,onFlighView})=>{
-    if(!airports||airports.length ===0){
-        return(
-            <p>no airports</p>
-        )
-    }
-
-    return(
-        <div className={styles.board}>
-            <div className={styles.header}>
-                <div>Name</div>
-                <div>code</div>
-                <div>City</div>
-                <div>actions</div>
-            </div>
-            <div className={styles.List}>
-                {airports.map(airport=>(
-                    <div key={airport.id} className={styles.Row}>
-                        <div className={styles.elements}> {airport.name}</div>
-                        <div className={styles.elements}> {airport.code}</div>
-                        <div className={styles.elements}>{airport.city.name} </div>
-                        <div>
-                        <button
+  return (
+    <div className={styles.board}>
+      <div className={styles.header}>
+        <div>Name</div>
+        <div>code</div>
+        <div>City</div>
+        <div>actions</div>
+      </div>
+      <div className={styles.List}>
+        {airports.map((airport) => (
+          <div key={airport.id} className={styles.Row}>
+            <div className={styles.elements}>{airport.name}</div>
+            <div className={styles.elements}>{airport.code}</div>
+            <div className={styles.elements}>{airport.city.name}</div>
+            <div className={styles.actionButtons}>
+              <button
                 className={styles.editButton}
                 onClick={() => onEdit(airport)}
                 title="Edit Airport"
               >
                 <FaEdit />
               </button>
-                        <button
+              <button
                 className={styles.deleteButton}
                 onClick={() => onDelete(airport.id)}
                 title="Delete Airport"
               >
                 <FaTrash />
               </button>
-              </div>
-                    </div>
-                ))}
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-}
-
-
-
-
-
-
-export default AirportTable
+export default AirportTable;
